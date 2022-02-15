8d7dcc0b7704abb970e006b1b8eeb34ec2232d8f
Avrpatel <avinash2772@gmail.com>
2022-02-14 22:00:33 -0500

    python loops activity and folder update

---
+++ Class-activities/python-1/Python Loops/cheer.py
@@ -0,0 +1,16 @@
+# Create a variable named cheer and give it a word to cheer (i.e. Python or FinTech)
+cheer = "python"
+
+# Below strings can be used to add fun
+cheer_symbol = "*\O/*"
+cheer_symbol_2 = "ヘ( ^o^)ノ＼(^_^ )"
+
+# Loop through string and print each letter with a cheer
+for x in cheer 
+print (cheer_symbol + x + cheer_symbol_2)
+
+# Print excitement to screen
+print("\nWhat does that spell?!")
+print(cheer + "!\nWoohoo! Go " + cheer + "!")
+print(cheer_symbol * 3)
+print(cheer_symbol_2)
