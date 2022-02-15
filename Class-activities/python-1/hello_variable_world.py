2dd23abb3d6ac4ec17ab35bfc26c6aa08ca20eee
Avrpatel <avinash2772@gmail.com>
2022-02-14 21:13:18 -0500

    Acitvites for python day 1 variables add

---
+++ Class-activities/python-1/hello_variable_world.py
@@ -0,0 +1,32 @@
+# Percent Increase Bonus Activity
+
+# Formulas
+Increase = Current_Price - Original_Price
+Percent_Increase = (Increase / Original_Price) x 100
+
+# Create float variable for original_price
+Original_Price = 198.87
+
+# Create float variable for current_price
+Current_Price = 254.32
+
+# Calculate difference between current_price and original_price
+
+Print (Increase)
+
+
+# Calculate percent_increase
+
+Print (Percent_Increase) 
+
+# Print original_price
+print(f"Apple's original stock price was ${Original_Price}")
+
+# Print current_price
+print(f"Apples current stock price is ${Current_Price}")
+
+# Print percent_increase to 2 decimal places using string formatting
+
+
+
+
