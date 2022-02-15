3d41accbaaba8aec6d0ca5fc95cd2133b60354a3
Avrpatel <avinash2772@gmail.com>
2022-02-14 22:32:52 -0500

    pseduo code activity for buy/sell stocks

---
+++ Class-activities/python-1/Python Pseudo Code/updated_conditionally_yours_part_1.py
@@ -0,0 +1,26 @@
+"""
+Conditionally Yours
+
+Pseudocode: 
+
+Declare your variables
+
+Original_Price 
+Current_Price
+Increase = Current_price - Original_Price
+Decrease = Orginal_price - Current_price 
+Pct_Threshold = 20%
+
+round(Pct_Increase = Increase / Orginal_price * 100,2) 
+round(Pct_Decrease = Decrease / Original_price * 100, 2)
+
+if Pct_Increase >= Pct_Threshold:
+    print ("Buy Stock")
+    
+else
+    print ("Sell Stock")
+
+ 
+
+
+"""
